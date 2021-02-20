daaf468ff4536b886247276416316f43ce232a9a
Zakaria AITOUMERIEM <zakariaaitoumeriem1889@gmail.com>
2021-02-20 23:45:59 +0100

    Registering users

---
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use Illuminate\Contracts\Foundation\Application;
+use Illuminate\Contracts\View\Factory;
+use Illuminate\Contracts\View\View;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Validation\ValidationException;
+
+/**
+ * Class RegisterController
+ * @package App\Http\Controllers\Auth
+ */
+class RegisterController extends Controller
+{
+    /**
+     * @return Application|Factory|View
+     */
+    public function index()
+    {
+        return view('auth.register');
+    }
+
+    /**
+     * @param Request $request
+     * @return RedirectResponse
+     * @throws ValidationException
+     */
+    public function store(Request $request)
+    {
+        $this->validate($request, [
+            'name' => 'required|max:255',
+            'username' => 'required|max:255',
+            'email' => 'required|email|max:255',
+            'password' => 'required|confirmed'
+        ]);
+        User::create([
+            'name' => $request->name,
+            'username' => $request->username,
+            'email' => $request->email,
+            'password' => Hash::make($request->password)
+        ]);
+        return redirect()->route('dashboard');
+    }
+}
---
+++ app/Http/Controllers/DashboardController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Contracts\Foundation\Application;
+use Illuminate\Contracts\View\Factory;
+use Illuminate\Contracts\View\View;
+use Illuminate\Http\Request;
+
+/**
+ * Class DashboardController
+ * @package App\Http\Controllers
+ */
+class DashboardController extends Controller
+{
+    /**
+     * @return Application|Factory|View
+     */
+    public function index()
+    {
+        return view('dashboard');
+    }
+}
