1f7cb34277d9290be15d5c16a7fa4667c52fb1fb
Zakaria AITOUMERIEM <zakariaaitoumeriem1889@gmail.com>
2021-02-20 21:51:18 +0100

    Styling the layout

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,7 +7,31 @@
     <link rel="stylesheet" href="{{asset('css/app.css')}}"/>
     <title>Posty</title>
 </head>
-<body>
+<body class="bg-gray-200">
+<nav class="p-6 bg-white flex justify-between mb-6">
+    <ul class="flex items-center">
+        <li>
+            <a href="" class="p-3">Home</a>
+        </li>
+        <li>
+            <a href="" class="p-3">Dashboard</a>
+        </li>
+        <li>
+            <a href="" class="p-3">Post</a>
+        </li>
+    </ul>
+    <ul class="flex items-center">
+        <li>
+            <a href="" class="p-3">Zakaria AITOUMERIEM</a>
+        </li>
+        <li>
+            <a href="" class="p-3">Login</a>
+        </li>
+        <li>
+            <a href="" class="p-3">Register</a>
+        </li>
+    </ul>
+</nav>
 @yield('content')
 </body>
 </html>
