834428b657c12cf3ec46dded67318795c98c364a
Zakaria AITOUMERIEM <zakariaaitoumeriem1889@gmail.com>
2021-02-22 00:34:36 +0100

    Eager loading

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -26,18 +26,20 @@
                         <a href="" class="font-bold">{{$post->user->name}}</a> <span class="text-gray-600 text-sm">{{$post->created_at->diffForHumans()}}</span>
                         <p class="mb-2">{{$post->body}}</p>
                         <div class="flex items-center">
-                            @if (!$post->likedBy(auth()->user()))
-                                <form action="{{route('posts.likes', $post->id)}}" method="post" class="mr-1">
-                                    @csrf
-                                    <button type="submit" class="text-blue-500">Like</button>
-                                </form>
-                            @else
-                                <form action="{{route('posts.likes', $post->id)}}" method="post" class="mr-1">
-                                    @csrf
-                                    @method('DELETE')
-                                    <button type="submit" class="text-red-500">Unlike</button>
-                                </form>
-                            @endif
+                            @auth()
+                                @if (!$post->likedBy(auth()->user()))
+                                    <form action="{{route('posts.likes', $post->id)}}" method="post" class="mr-1">
+                                        @csrf
+                                        <button type="submit" class="text-blue-500">Like</button>
+                                    </form>
+                                @else
+                                    <form action="{{route('posts.likes', $post->id)}}" method="post" class="mr-1">
+                                        @csrf
+                                        @method('DELETE')
+                                        <button type="submit" class="text-red-500">Unlike</button>
+                                    </form>
+                                @endif
+                            @endauth
                             <span>
                                 {{$post->likes->count()}} {{Str::plural('like', $post->likes->count())}}
                             </span>
