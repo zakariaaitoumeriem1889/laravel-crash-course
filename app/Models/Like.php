6203102c16f4b22b3de2231f5d01676779f48449
Zakaria AITOUMERIEM <zakariaaitoumeriem1889@gmail.com>
2021-02-28 20:58:47 +0100

    Soft deleting models

--- app/Models/Like.php
+++ app/Models/Like.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 /**
  * Class Like
@@ -11,7 +12,7 @@ use Illuminate\Database\Eloquent\Model;
  */
 class Like extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes;
 
     /**
      * @var string[]
